ae9f7c7d2cab6634c514a9a74366c7071beec314
ccshare <chen.cheng@myshare.io>
2018-10-27 23:28:24 +0800

    Add code

--- server/server.go
+++ server/server.go
@@ -1,9 +1,74 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
 )
 
+func echoClientAddress(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	remoteIP := r.RemoteAddr
+	if index := strings.Index(r.RemoteAddr, ":"); index > 0 {
+		remoteIP = r.RemoteAddr[:index]
+	}
+	data, _ := json.Marshal(map[string]interface{}{"ip": remoteIP})
+	w.Write(data)
+}
+
+func pingClient(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	if r.Method == "POST" {
+		w.Header().Set("Content-Type", "application/json")
+
+		data := map[string]interface{}{}
+
+		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+			log.Println("decode json error: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("failed"))
+			return
+		}
+		remoteIP := r.RemoteAddr
+		if index := strings.Index(r.RemoteAddr, ":"); index > 0 {
+			remoteIP = r.RemoteAddr[:index]
+		}
+		clientURL := fmt.Sprintf("%s:%s", remoteIP, data["port"])
+		http.Post(clientURL, "application/json", r.Body)
+	}
+}
+
 func main() {
-	fmt.Println("server");
+	fport := flag.Uint("port", 23456, "Listen port")
+	flag.Parse()
+	addrPort := fmt.Sprintf(":%d", *fport)
+
+	http.HandleFunc("/ip", echoClientAddress)
+	http.HandleFunc("/ping", pingClient)
+
+	//http.Handle("/pkgs/", http.StripPrefix("/pkgs/", http.FileServer(http.Dir(*filedir))))
+
+	http.HandleFunc("/upnp", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("upnp"))
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<a href=\"/ip\">Get public ip</a>\n"))
+		w.Write([]byte("<br>\n"))
+		w.Write([]byte("<a href=\"/upnp\">Test upnp</a>\n"))
+	})
+
+	fmt.Printf("server: %s", addrPort)
+	err := http.ListenAndServe(addrPort, nil)
+	if err != nil {
+		log.Println("listen error: ", err)
+	}
 }
--- upnp.go
+++ upnp.go
@@ -4,12 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
+	"strconv"
 
 	"github.com/NebulousLabs/go-upnp"
 )
 
 func main() {
-	fport := flag.Uint("port", 9902, "Listen port")
+	fport := flag.Uint("port", 23456, "Listen port")
 	flag.Parse()
 	port := uint16(*fport)
 
@@ -32,6 +35,37 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("unmap port:%d success\n", port)
+
+	reqPath := "http://192.168.56.1"
+	urlPath, err := url.Parse(reqPath)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	params := url.Values{}
+	params.Set("port", strconv.Itoa(int(port)))
+	params.Set("token", "upnp-test")
+	urlPath.RawQuery = params.Encode()
+	resp, err := http.Get(urlPath.String())
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalln("ping server failed: ", resp.Status)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+		}
+	})
+
+	addrPort := fmt.Sprintf(":%d", port)
+	fmt.Printf("server: %s", addrPort)
+	err = http.ListenAndServe(addrPort, nil)
+	if err != nil {
+		log.Println("listen error: ", err)
+	}
 
 	// discover external IP
 	ip, err := d.ExternalIP()

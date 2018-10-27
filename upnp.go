4e13235f911d2943bfa7a9e8e76aab0f037777af
ccshare <chen.cheng@myshare.io>
2018-10-27 14:28:50 +0800

    Add upnp code

--- upnp.go
+++ upnp.go
@@ -1,9 +1,52 @@
 package main
 
 import (
-  "fmt"
-  )
-  
- func main() {
-   fmt.Println("Hello");
-   }
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/NebulousLabs/go-upnp"
+)
+
+func main() {
+	fport := flag.Uint("port", 9902, "Listen port")
+	flag.Parse()
+	port := uint16(*fport)
+
+	// connect to router
+	d, err := upnp.Discover()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// forward a port
+	err = d.Forward(port, "myshare-upnp-checker")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("map port:%d success\n", port)
+
+	// un-forward a port
+	err = d.Clear(port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("unmap port:%d success\n", port)
+
+	// discover external IP
+	ip, err := d.ExternalIP()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Your external IP is:", ip)
+
+	// record router's location
+	loc := d.Location()
+	fmt.Println("router upnp info: ", loc)
+
+	// connect to router directly
+	d, err = upnp.Load(loc)
+	if err != nil {
+		log.Fatal(err)
+	}
+}

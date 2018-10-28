68311910922681f34a9f927bb33d3bad08638ecf
ccshare <chen.cheng@myshare.io>
2018-10-28 20:26:32 +0800

    Refine

--- upnp.go
+++ upnp.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -18,6 +19,7 @@ import (
 var Version = "1.0.3"
 
 // ServerURL default Server URL
+//var ServerURL = "http://192.168.56.1:23456/ping"
 var ServerURL = "http://www.aweg.cc:23456/ping"
 
 func newListener() net.Listener {
@@ -43,34 +45,38 @@ func clearPort(d *upnp.IGD, port uint16) {
 }
 
 func main() {
-	checkResult := false
+	successResult := false
+	waitSec := flag.Duration("wait", 5, "wait seconds")
+	flag.Parse()
 
 	// connect to router
 	d, err := upnp.Discover()
 	if err != nil {
 		log.Fatal("Not find upnp router, ", err)
 	}
-	fmt.Printf("Time and Version  : %s %s\n", time.Now().Format("2006-01-02 15:04:05"), Version)
+	fmt.Printf("Time and Version  : %s @ %s\n", time.Now().Format("2006-01-02 15:04:05"), Version)
 	// record router's location
 	loc := d.Location()
 	fmt.Printf("Router locaion    : %s\n", loc)
 
 	token := fmt.Sprintf("%d", time.Now().UnixNano())
-	srv := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("test server"))
-		if r.Method == "GET" {
-			r.ParseForm()
-			if token != r.Form.Get("token") {
-				log.Printf("Unexpected token  : %s\n", r.Form.Get("token"))
+	srv := &httptest.Server{
+		Listener: newListener(),
+		Config: &http.Server{Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("test server"))
+			if r.Method == "GET" {
+				r.ParseForm()
+				if token != r.Form.Get("token") {
+					log.Printf("Unexpected token  : %s\n", r.Form.Get("token"))
+				}
+				fmt.Printf("Real public IP    : %v\n", r.Form.Get("ip"))
+				successResult = true
+			} else {
+				log.Printf("Unexpected request:%s\n", r.Method)
 			}
-			fmt.Printf("Real public IP    : %v\n", r.Form.Get("ip"))
-			checkResult = true
-		} else {
-			log.Printf("Unexpected request:%s\n", r.Method)
-		}
-	}))
-	srv.Listener = newListener()
+		})},
+	}
 	srv.Start()
 
 	portIndex := strings.LastIndex(srv.URL, ":")
@@ -116,18 +122,20 @@ func main() {
 		fmt.Scanln()
 		return
 	}
-
-	time.Sleep(2000)
-	clearPort(d, uint16(iport))
 	// discover external IP
 	ip, err := d.ExternalIP()
 	if err != nil {
 		log.Println("upnp public IP err: ", err)
+		clearPort(d, uint16(iport))
 		fmt.Scanln()
 		return
 	}
 	fmt.Printf("upnp public IP    : %s\n", ip)
-	if checkResult {
+
+	time.Sleep(*waitSec * time.Second)
+	clearPort(d, uint16(iport))
+
+	if successResult {
 		fmt.Println("Success!")
 	} else {
 		fmt.Println("Failed!")

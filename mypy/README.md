13b320748bc9bc3cfea36b605ae50c84c55d8845
shvc <chen.cheng@myshare.io>
2023-03-29 23:24:05 +0800

    fastapi

---
+++ mypy/README.md
@@ -0,0 +1,4 @@
+## fastapi
+
+uvicorn main:app --host 0.0.0.0 --port 80 --reload
+
---
+++ mypy/main.py
@@ -0,0 +1,47 @@
+
+
+import os
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse, JSONResponse
+import uvicorn
+import shutil
+
+store = os.getcwd()
+
+app = FastAPI()
+
+
+@app.put("/file/{filename}")
+async def put_file(filename: str, req: Request):
+    with open(os.path.join(store, filename), "wb") as buffer:
+        content = await req.body()
+        buffer.write(content)
+        buffer.close()
+    return JSONResponse(content={"filename": filename}, status_code=200)
+
+
+@app.get('/file')
+async def list_file():
+    return JSONResponse(content={"file": os.listdir(store)}, status_code=200) 
+
+
+@app.get('/file/{filename}')
+async def get_file(filename: str):
+    return FileResponse(path=os.path.join(store, filename), filename=filename)
+
+
+@app.delete('/file/{filename}')
+async def delete_file(filename: str):
+    try:
+        os.remove(os.path.join(store, filename))
+        return JSONResponse(content={
+            "removed": True
+        }, status_code=200)
+    except FileNotFoundError:
+        return JSONResponse(content={
+            "removed": False,
+            "error_message": "File not found"
+        }, status_code=404)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=80)
